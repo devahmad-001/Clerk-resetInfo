ae574fbeca0b1325c662fc6d105583982967d786
FARHAN AHMAD <117432911+789farhan@users.noreply.github.com>
2024-07-10 14:32:04 +0500

    user-authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ProviderComp } from "@/provider";
-
+import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ProviderComp>{children}</ProviderComp>
+        <ClerkProvider>{children}</ClerkProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-import FormData from "@/components/FormData";
-
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { useClerk } from "@clerk/nextjs";
+import { SignOutButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 export default function Home() {
-  return <>
-  <FormData/>
-  </>;
+  const { signOut } = useClerk();
+  const { isSignedIn, user, isLoaded } = useUser();
+  const Navigate = useRouter();
+  return (
+    <>
+      {/* User can manage account info like email,name,password */}
+
+      {/* @ts-ignore */}
+
+      <UserButton />
+
+      {/* After signout nevigate to signin page   */}
+
+      {/* {isSignedIn ? <UserButton /> : Navigate.push("/signin")} */}
+
+      {/* After signout nevigate to signin page   */}
+
+      {/* {isSignedIn && (
+        <button onClick={() => signOut({ redirectUrl: "/signin" })}>
+          Sign out
+        </button>
+      )} */}
+    </>
+  );
 }
